09224f86c67ef67b2019b85c0148622bd382a258
gyuetorade <159615822+gyuetorade@users.noreply.github.com>
2024-02-22 13:52:21 +0800

    Update movement.py

--- movement.py
+++ movement.py
@@ -3,13 +3,13 @@ import pygame
 def set_direction(character, keys):
     if keys[pygame.K_UP]:
         character.set_direction("up")
-        character.rect.y -= 10  # Adjust the movement speed as needed
+        character.rect.y -= 7  # Adjust the movement speed as needed
     elif keys[pygame.K_DOWN]:
         character.set_direction("down")
-        character.rect.y += 10  # Adjust the movement speed as needed
+        character.rect.y += 7  # Adjust the movement speed as needed
     elif keys[pygame.K_LEFT]:
         character.set_direction("left")
-        character.rect.x -= 10  # Adjust the movement speed as needed
+        character.rect.x -= 7  # Adjust the movement speed as needed
     elif keys[pygame.K_RIGHT]:
         character.set_direction("right")
-        character.rect.x += 10  # Adjust the movement speed as needed
+        character.rect.x += 7  # Adjust the movement speed as needed
