a9c330e9da8d1628913f183828b874c2ba5190e1
gyue <hanslester04@gmail.com>
2024-02-25 14:32:18 +0800

    movement edits still not working

--- fishing 3.py
+++ fishing 3.py
@@ -81,7 +81,7 @@ class Game:
 
 
         # Draw the character's and path mask on the screen for debugging
-        SCREEN.blit(p   ygame.Surface(self.character.rect.size, pygame.SRCALPHA), self.character.rect)
+        SCREEN.blit(pygame.Surface(self.character.rect.size, pygame.SRCALPHA), self.character.rect)
         SCREEN.blit(pygame.Surface(self.path_mask.get_rect().size, pygame.SRCALPHA), self.path_mask.get_rect())
 
         pass
@@ -96,19 +96,14 @@ def main_menu():
         SCREEN.blit(BG, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
-
         MENU_TEXT = get_font(75).render("Life Under the Sea", True, (100, 200, 50))
-
         PLAY_BUTTON = Button(r"assets\Button\Button_Play.png", (SCREEN_WIDTH // 2, 450))
-
         QUIT_BUTTON = Button(r"assets\Button\Button_Quit.png", (SCREEN_WIDTH // 2, 550))
 
         SCREEN.blit(MENU_TEXT, (50, 100))
 
         for button in [PLAY_BUTTON, QUIT_BUTTON]:
-            button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -116,7 +111,6 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     in_game = True  # Set in_game to True when PLAY is pressed
-
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
--- movement.py
+++ movement.py
@@ -1,15 +1,18 @@
 import pygame
 
 def set_direction(character, keys):
-    if keys[pygame.K_UP]:
+    # Adjust the movement speed as needed
+    speed = 7
+
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
         character.set_direction("up")
-        character.rect.y -= 7  # Adjust the movement speed as needed
-    elif keys[pygame.K_DOWN]:
+        character.rect.y -= speed
+    elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
         character.set_direction("down")
-        character.rect.y += 7  # Adjust the movement speed as needed
-    elif keys[pygame.K_LEFT]:
+        character.rect.y += speed
+    elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
         character.set_direction("left")
-        character.rect.x -= 7  # Adjust the movement speed as needed
-    elif keys[pygame.K_RIGHT]:
+        character.rect.x -= speed
+    elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         character.set_direction("right")
-        character.rect.x += 7  # Adjust the movement speed as needed
+        character.rect.x += speed
